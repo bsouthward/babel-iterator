d296965240cec017f583abd43a2fa6300a08e630
Brien Southward <briensouthward@Briens-MacBook-Pro.local>
2018-08-25 10:35:31 -0700

    updated

--- babel2.py
+++ babel2.py
@@ -7,19 +7,23 @@ class Babel:
 		return self
 
 	def __next__(self):
-
+		# A couple helper functions
+		# This one generates an 'alphabet'
 		def get_alpha(n):
 			alphabet = []
 			for c in range(0, 2**n):
 				alphabet.append(chr(c))
 			return alphabet
 
+		# And I defined a powerset function instead of using itertools
+		# to keep the class self-contained
 		def powerset(s):
 			x = len(s)
 			masks = [1 << i for i in range(x)]
 			for i in range(1 << x):
 				yield [ss for mask, ss in zip(masks, s) if i & mask]
 
+		# Do the thing if something isn't broken
 		if self.current > self.high:
 			raise StopIteration
 		else:
@@ -27,5 +31,6 @@ class Babel:
 			self.current += 1
 			return library
 
+# Even just n = 2 results in a pretty big combinatorial space!
 for b in Babel(2):
 	print(b)
